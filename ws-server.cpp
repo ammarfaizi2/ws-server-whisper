a0b9f8f183187705841d86daf442f99b9bd4a7b4
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-03-02 01:46:29 +0700

    Better event handling, add on_accept and on_close
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- ws-server.cpp
+++ ws-server.cpp
@@ -1,5 +1,6 @@
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
+#include <unordered_map>
 #include <iostream>
 #include <fstream>
 #include <cstdint>
@@ -21,19 +22,141 @@ using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 
-static wav_writer g_ww;
-
 struct ws_server {
 	server ctx;
 };
 
+struct ws_client_slot {
+	std::string		fname;
+	wav_writer		ww;
+	std::vector<float>	pcmf32;
+};
+
+#define WHISPER_SAMPLE_RATE 44100
+
+struct whisper_params {
+	int32_t n_threads  = std::min(4, (int32_t) std::thread::hardware_concurrency());
+	int32_t step_ms    = 3000;
+	int32_t length_ms  = 10000;
+	int32_t keep_ms    = 200;
+	int32_t capture_id = -1;
+	int32_t max_tokens = 32;
+	int32_t audio_ctx  = 0;
+
+	float vad_thold    = 0.6f;
+	float freq_thold   = 100.0f;
+
+	bool speed_up      = false;
+	bool translate     = false;
+	bool no_fallback   = false;
+	bool print_special = false;
+	bool no_context    = true;
+	bool no_timestamps = false;
+	bool tinydiarize   = false;
+	bool save_audio    = false; // save audio to wav file
+	bool use_gpu       = true;
+
+	std::string language  = "en";
+	std::string model     = "models/ggml-base.en.bin";
+	std::string fname_out;
+};
+
+static whisper_params params;
+static std::mutex g_ws_clients_mutex;
+static std::unordered_map<void *, ws_client_slot *> g_ws_clients;
+
+static ws_client_slot *find_ws_client(websocketpp::connection_hdl hdl)
+{
+	std::lock_guard<std::mutex> lock(g_ws_clients_mutex);
+
+	auto it = g_ws_clients.find(hdl.lock().get());
+	if (it != g_ws_clients.end())
+		return it->second;
+
+	return nullptr;
+}
+
+static ws_client_slot *add_ws_client(websocketpp::connection_hdl hdl)
+{
+	std::lock_guard<std::mutex> lock(g_ws_clients_mutex);
+	void *key = hdl.lock().get();
+	ws_client_slot *cl;
+
+	auto it = g_ws_clients.find(key);
+	if (it != g_ws_clients.end())
+		return it->second;
+
+	cl = new ws_client_slot;
+	g_ws_clients[key] = cl;
+	return cl;
+}
+
+static void del_ws_client(websocketpp::connection_hdl hdl)
+{
+	std::lock_guard<std::mutex> lock(g_ws_clients_mutex);
+	void *key = hdl.lock().get();
+	ws_client_slot *cl;
+
+	auto it = g_ws_clients.find(key);
+	if (it != g_ws_clients.end()) {
+		cl = it->second;
+		g_ws_clients.erase(it);
+		delete cl;
+	}
+}
+
+static void on_accept(struct ws_server *s, websocketpp::connection_hdl hdl)
+{
+	const int n_samples_step = (1e-3*params.step_ms  )*WHISPER_SAMPLE_RATE;
+	const int n_samples_len  = (1e-3*params.length_ms)*WHISPER_SAMPLE_RATE;
+	const int n_samples_keep = (1e-3*params.keep_ms  )*WHISPER_SAMPLE_RATE;
+	const int n_samples_30s  = (1e-3*30000.0         )*WHISPER_SAMPLE_RATE;
+
+	ws_client_slot *cl = add_ws_client(hdl);
+
+	std::string ep = s->ctx.get_con_from_hdl(hdl)->get_remote_endpoint();
+	std::string fname = ep + ".wav";
+
+	// Replace ':' with '_'
+	for (size_t i = 0; i < fname.size(); i++) {
+		if (fname[i] == ':')
+			fname[i] = '_';
+	}
+
+	std::cout << "Creating file " << fname << std::endl;
+	cl->ww.open(fname, WHISPER_SAMPLE_RATE, 16, 1);
+	cl->fname = fname;
+	cl->pcmf32 = std::vector<float>(n_samples_30s, 0.0f);
+	cl->ww.write(cl->pcmf32.data(), cl->pcmf32.size());
+}
+
 static void on_message(struct ws_server *s, websocketpp::connection_hdl hdl, msg_ptr msg)
 {
+	std::string ep = s->ctx.get_con_from_hdl(hdl)->get_remote_endpoint();
+	ws_client_slot *cl = find_ws_client(hdl);
+
+	if (!cl)
+		return;
+
 	const auto &payload = msg->get_payload();
 	size_t len = payload.size();
 
-	std::cout << "Client " << hdl.lock().get() << " sent " << len << " bytes" << std::endl;
-	g_ww.write(reinterpret_cast<const float *>(payload.data()), len / sizeof(float));
+	cl->pcmf32.clear();
+	cl->pcmf32.resize(len/sizeof(float));
+	memcpy(cl->pcmf32.data(), payload.data(), len);
+	cl->ww.write(cl->pcmf32.data(), cl->pcmf32.size());
+	std::cout << "Received " << len << " bytes from " << ep << std::endl;
+
+	// for (size_t i = 0; i < len/sizeof(float); i++)
+	// 	std::cout << cl->pcmf32[i] << " ";
+
+	// std::cout << std::endl;
+}
+
+static void on_close(struct ws_server *s, websocketpp::connection_hdl hdl)
+{
+	std::cout << "Client " << hdl.lock().get() << " disconnected" << std::endl;
+	del_ws_client(hdl);
 }
 
 static void run_ws_server(const char *addr, uint16_t port)
@@ -49,7 +172,9 @@ static void run_ws_server(const char *addr, uint16_t port)
 	ws.ctx.init_asio();
 
 	// Register our message handler
+	ws.ctx.set_open_handler(bind(&on_accept, &ws, ::_1));
 	ws.ctx.set_message_handler(bind(&on_message, &ws, ::_1, ::_2));
+	ws.ctx.set_close_handler(bind(&on_close, &ws, ::_1));
 	ws.ctx.listen(ep);
 	ws.ctx.start_accept();
 	ws.ctx.run();
@@ -58,11 +183,6 @@ static void run_ws_server(const char *addr, uint16_t port)
 int main(void)
 {
 	try {
-		if (!g_ww.open("output.wav", 16000, 16, 1)) {
-			std::cerr << "Failed to open output.wav" << std::endl;
-			return 1;
-		}
-
 		run_ws_server("0.0.0.0", 9002);
 		return 0;
 	} catch (const std::exception & e) {
